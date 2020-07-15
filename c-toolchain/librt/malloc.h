e0422640ab292338d3a5b67fc7a85abcbea7b0dc
natanbc <natanbc@users.noreply.github.com>
2020-07-14 21:11:38 -0300

    move malloc initialization to __start if enabled

--- c-toolchain/librt/malloc.h
+++ c-toolchain/librt/malloc.h
@@ -2,12 +2,6 @@
 
 #include "stddef.h"
 
-//allows for smaller binaries for programs that don't use malloc
-
-//could maybe be moved to a compile-time define?
-//make WITH_MALLOC=1 binary?
-void rt_malloc_init(void);
-
 void* malloc(size_t size);
 void free(void* ptr);
 
